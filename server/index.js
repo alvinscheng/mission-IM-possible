52db459b5f015b92c50e0e61aac90ff7b15d1b59
Alvin Cheng <ascheng21@gmail.com>
2017-08-08 10:44:26 -0700

    Added a connection event handler. #2

--- server/index.js
+++ server/index.js
@@ -4,9 +4,13 @@ const path = require('path')
 
 const app = express()
 const server = require('http').createServer(app)
+const io = require('socket.io')(server)
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-const port = process.env.PORT || 3000
+io.on('connection', socket => {
+  console.log('a user connected')
+})
 
+const port = process.env.PORT || 3000
 server.listen(port, () => console.log('Listening on ' + port))

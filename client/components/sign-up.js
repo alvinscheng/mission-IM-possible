0ab5cca323fcb228d090e9fe61305e1f507195c5
Alvin Cheng <ascheng21@gmail.com>
2017-08-10 15:43:09 -0700

    Used regex to validate username and password. #8

--- client/components/sign-up.js
+++ client/components/sign-up.js
@@ -7,17 +7,41 @@ const SignupForm = () => {
       onSubmit={(values) => {
         console.log('Success!', values)
       }}
-      validate={({ name }) => {
+      validate={({ username, password }) => {
         return {
-          name: !name ? 'A name is required' : undefined
+          username: !username ? 'A username is required' : null,
+          password: (!password || password.length < 6)
+          ? 'A password of 6 or more characters is required'
+          : (!password.match(/(?=.*\d)(?=.*[a-z])/))
+            ? 'Password must include at least 1 letter and 1 number'
+            : null
         }
       }}
     >
       {({submitForm}) => {
         return (
           <form onSubmit={submitForm}>
-            <Text field='name' />
-            <button type='submit'>Submit</button>
+            <label>
+              <span>Username:</span>
+              <Text
+                field='username'
+                className='form-control'
+              />
+            </label>
+            <label>
+              <span>Password:</span>
+              <Text
+                field='password'
+                type='password'
+                className='form-control'
+              />
+            </label>
+            <button
+              type='submit'
+              className='btn btn-form btn-default'
+            >
+              Create Account
+            </button>
           </form>
         )
       }}
